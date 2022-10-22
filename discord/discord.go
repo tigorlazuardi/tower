1b1a48cd1eb8bfddad76cfe3eb58d3f233d05466
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-10-22 10:35:56 +0700

    feat: update messenger spec

---
+++ discord/discord.go
@@ -0,0 +1,32 @@
+package discord
+
+import (
+	"context"
+
+	"github.com/tigorlazuardi/tower"
+)
+
+type Discord struct {
+	name  string
+	token string
+}
+
+// Returns the name of the Messenger.
+func (d Discord) Name() string {
+	if d.name == "" {
+		return "discord"
+	}
+	return d.name
+}
+
+// Sends notification.
+func (d Discord) SendMessage(ctx tower.MessageContext) error {
+	panic("not implemented") // TODO: Implement
+}
+
+// Waits until all message in the queue or until given channel is received.
+//
+// Implementer must exit the function as soon as possible when this context receivied the cancel context.
+func (d Discord) Wait(ctx context.Context) error {
+	return nil
+}
--- messenger.go
+++ messenger.go
@@ -1,5 +1,7 @@
 package tower
 
+import "context"
+
 type Messenger interface {
 	// Returns the name of the Messenger.
 	Name() string
@@ -8,6 +10,6 @@ type Messenger interface {
 
 	// Waits until all message in the queue or until given channel is received.
 	//
-	// Implementer must exit the function as soon as possible when this cancel receives the item.
-	Wait(cancel <-chan struct{}) error
+	// Implementer must exit the function as soon as possible when this ctx is canceled.
+	Wait(ctx context.Context) error
 }

469ec01838476fc971e90e9de23cd1303890060e
tigorhutasuhut <tigor.hutasuhut@gmail.com>
2022-11-27 20:24:40 +0700

    refactor(caller): caller is now an interface

--- caller.go
+++ caller.go
@@ -11,32 +11,41 @@ import (
 
 const sep = string(os.PathSeparator)
 
-type Caller struct {
+type Caller interface {
+	Function() *runtime.Func
+	Origin() string
+	ShortOrigin() string
+	ShortSource() string
+	String() string
+	FormatAsKey() string
+}
+
+type caller struct {
 	PC   uintptr
 	File string
 	Line int
 }
 
-func (c Caller) MarshalJSON() ([]byte, error) {
+func (c caller) MarshalJSON() ([]byte, error) {
 	return json.Marshal(c.String())
 }
 
-func (c Caller) Function() *runtime.Func {
+func (c caller) Function() *runtime.Func {
 	f := runtime.FuncForPC(c.PC)
 	return f
 }
 
-func (c Caller) Origin() string {
+func (c caller) Origin() string {
 	f := runtime.FuncForPC(c.PC)
 	return f.Name()
 }
 
-func (c Caller) getOrigin() []string {
+func (c caller) getOrigin() []string {
 	return strings.Split(c.Origin(), "/")
 }
 
 // ShortOrigin returns only the latest four items maximum in the package path.
-func (c Caller) ShortOrigin() string {
+func (c caller) ShortOrigin() string {
 	s := c.getOrigin()
 
 	for len(s) > 3 {
@@ -46,8 +55,8 @@ func (c Caller) ShortOrigin() string {
 	return strings.Join(s, "/")
 }
 
-// ShortSource Gets only the latest three items path in the File Path where the Caller comes from.
-func (c Caller) ShortSource() string {
+// ShortSource returns only the latest three items path in the File Path where the Caller comes from.
+func (c caller) ShortSource() string {
 	s := strings.Split(c.File, sep)
 
 	for len(s) > 3 {
@@ -58,7 +67,7 @@ func (c Caller) ShortSource() string {
 }
 
 // FormatAsKey Like .String(), but runes other than letters, digits, `-` and `.` are set to `_`.
-func (c Caller) FormatAsKey() string {
+func (c caller) FormatAsKey() string {
 	s := &strings.Builder{}
 	strLine := strconv.Itoa(c.Line)
 	s.Grow(len(c.File) + 1 + len(strLine))
@@ -68,8 +77,8 @@ func (c Caller) FormatAsKey() string {
 	return s.String()
 }
 
-// Sets this caller as `file_path:line` format.
-func (c Caller) String() string {
+// String Sets this caller as `file_path:line` format.
+func (c caller) String() string {
 	s := &strings.Builder{}
 	strLine := strconv.Itoa(c.Line)
 	s.Grow(len(c.File) + 1 + len(strLine))
@@ -90,13 +99,13 @@ func replaceSymbols(builder *strings.Builder, s string, rep rune) {
 	}
 }
 
-// GetCaller Gets the caller information for who calls this function. A value of 1 will return this GetCaller location.
+// GetCaller returns the caller information for who calls this function. A value of 1 will return this GetCaller location.
 // So you may want the value to be 2 or higher if you wrap this call in another function.
 //
 // Returns zero value if the caller information cannot be obtained.
 func GetCaller(depth int) Caller {
 	pc, file, line, _ := runtime.Caller(depth)
-	return Caller{
+	return &caller{
 		PC:   pc,
 		File: file,
 		Line: line,

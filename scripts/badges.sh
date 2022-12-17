d547d7413dc6f2d83b3353f45322404c653d777e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-12-17 14:13:58 +0700

    feat(badges): badges are now generated dynamically for each packages

---
+++ scripts/badges.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+go test -coverprofile=coverage.out .
+COVERAGE=$(go tool cover -func=coverage.out | grep total: | grep -Eo '[0-9]+\.[0-9]+')
+COLOR=orange
+if (($(echo "$COVERAGE <= 50" | bc -l))); then
+	COLOR=red
+elif (($(echo "$COVERAGE > 80" | bc -l))); then
+	COLOR=green
+fi
+BADGE_URI=$(echo "tower-$COVERAGE%25-$COLOR" | sed -r 's/\s+/%20/g')
+URL="https://img.shields.io/badge/$BADGE_URI.svg"
+echo "Creating Badge for Tower: $URL"
+curl -sSL $URL --create-dirs -o "./dist/tower.svg"
+
+PACKAGES=$(find . -name go.mod | grep -v '^\./go.mod$' | cut -d/ -f2- | xargs dirname)
+for pkg in $PACKAGES; do
+	echo "Checking ./$pkg"
+	go test -coverprofile=coverage.out ./$pkg
+	# go tool cover -func=coverage.out
+	COVERAGE=$(go tool cover -func=coverage.out | grep total: | grep -Eo '[0-9]+\.[0-9]+')
+	COLOR=orange
+	if (($(echo "$COVERAGE <= 50" | bc -l))); then
+		COLOR=red
+	elif (($(echo "$COVERAGE > 80" | bc -l))); then
+		COLOR=green
+	fi
+	BADGE_URI=$(echo "$pkg-$COVERAGE%25-$COLOR" | sed -r 's/\s+/%20/g')
+	URL="https://img.shields.io/badge/$BADGE_URI.svg"
+	echo "Creating Badge for $pkg: $URL"
+	curl -sSL $URL --create-dirs -o "./dist/$pkg.svg"
+done
--- towerdiscord/integration_test.go
+++ towerdiscord/integration_test.go
@@ -97,6 +97,9 @@ func (f foo) Error() string {
 func TestIntegration(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+	if os.Getenv("DISCORD_TEST_INTEGRATION") == "" {
+		t.Skip("DISCORD_TEST_INTEGRATION is not set")
+	}
 	if testing.Short() {
 		t.Skip("Skipping integration test in short mode")
 	}

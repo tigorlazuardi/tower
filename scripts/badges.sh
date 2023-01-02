50cd2531e77b5a15cb65b78551044b87b435cfd9
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2023-01-02 12:01:10 +0700

    fix(drone): minio client now does not remove on mirror opts [CI SKIP]

--- scripts/badges.sh
+++ scripts/badges.sh
@@ -32,8 +32,5 @@ for pkg in $PACKAGES; do
 	echo "Checking ./$pkg"
 	go test -coverprofile=coverage.out ./$pkg >/dev/null
 	COV=$(go tool cover -func=coverage.out | grep total: | grep -Eo '[0-9]+\.[0-9]+')
-	if (($(echo "$COV < 1" | bc -l))); then
-		continue
-	fi
 	build_badge $pkg $COV
 done

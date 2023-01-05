291a2e6b82bdfae6080b495a7975153b95b155e6
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2023-01-05 15:28:10 +0700

    feat(minio): added minio bucket

---
+++ bucket/minio/v7/minio_integration_test.go
@@ -0,0 +1,40 @@
+package minio
+
+import (
+	"context"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/tigorlazuardi/tower/bucket"
+	"github.com/tigorlazuardi/tower/loader"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+	loader.LoadEnv()
+	if os.Getenv("MINIO_TEST_ENDPOINT") == "" {
+		t.Skip("skipping integration test")
+	}
+	client, err := minio.New(os.Getenv("MINIO_TEST_ENDPOINT"), &minio.Options{
+		Creds:  credentials.NewStaticV4(os.Getenv("MINIO_TEST_ACCESS_KEY"), os.Getenv("MINIO_TEST_SECRET_KEY"), ""),
+		Secure: true,
+	})
+	if err != nil {
+		t.Fatalf("could not create minio client: %v", err)
+	}
+	m := Wrap(client, os.Getenv("MINIO_TEST_BUCKET"))
+	file := bucket.NewFile(strings.NewReader("hello world"), "text/plain; charset=utf-8", bucket.WithFilename("test.txt"))
+	results := m.Upload(context.Background(), []bucket.File{file})
+	if len(results) == 0 {
+		t.Fatal("expected results")
+	}
+	for _, result := range results {
+		if result.Error != nil {
+			t.Fatalf("unexpected error: %v", result.Error)
+		}
+	}
+}

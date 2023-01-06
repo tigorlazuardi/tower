1bbfa03e350bf91b6142803ad07a4193c0937231
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2023-01-06 11:07:36 +0700

    feat(autotag): added go mod tidy steps

--- scripts/autotag.sh
+++ scripts/autotag.sh
@@ -8,23 +8,23 @@ IFS='.' read -a vers <<<"$CURTAG"
 MAJ=${vers[0]}
 MIN=${vers[1]}
 PATCH=${vers[2]}
-echo "Current Tag: $MAJ.$MIN.$PATCH"
+echo "== INFO: Current Tag: $MAJ.$MIN.$PATCH"
 
 case "$DRONE_COMMIT_MESSAGE" in
 *"#major"*)
 	((MAJ += 1))
 	MIN=0
 	PATCH=0
-	echo "#major found, incrementing major version"
+	echo "== INFO: #major found, incrementing major version"
 	;;
 *"#minor"*)
 	((MIN += 1))
 	PATCH=0
-	echo "#minor found, incrementing minor version"
+	echo "== INFO: #minor found, incrementing minor version"
 	;;
 *)
 	((PATCH += 1))
-	echo "incrementing Patch Version"
+	echo "== INFO: incrementing Patch Version"
 	;;
 esac
 
@@ -36,7 +36,18 @@ git add .
 FILES=$(find . -name go.mod | grep -v '^\./go.mod$')
 
 for f in $FILES; do
+  # --- Go Mod Tidy ---
+	PACKAGE=$(echo $f | cut -d/ -f2- | xargs dirname)
+  echo "== INFO: go mod tidy $PACKAGE"
+	cd $PACKAGE
+	GOSUMDB=off go mod tidy
+	git add go.mod go.sum || true
+	echo "== INFO: finished go mod tidy $PACKAGE"
+	cd -
+	# --- Update Tag in go.mod ---
+	echo "== INFO: Updating $PACKAGE to $NEWTAG"
 	sed -i -r "s#(\s+github\.com/tigorlazuardi/tower.*\sv).*#\1$NEWTAG#" $f
+	echo "== INFO: finished updating $PACKAGE to $NEWTAG"
 	git add $f
 done
 
@@ -45,11 +56,11 @@ git commit -m "Bump Version to v$NEWTAG [CI SKIP]"
 for f in $FILES; do
 	PACKAGE=$(echo $f | cut -d/ -f2- | xargs dirname)
 	PACKAGE_TAG="$PACKAGE/v$NEWTAG"
-	echo "Adding Tag: $PACKAGE_TAG"
+	echo "== INFO: Adding Tag: $PACKAGE_TAG"
 	git tag $PACKAGE_TAG
 done
 
-echo "Adding Tag: v$NEWTAG"
+echo "== INFO: Adding Tag: v$NEWTAG"
 git tag v$NEWTAG
 
 git push --force origin main
